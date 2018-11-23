786b0ddbafdc7a2616f44bc279c029621d1c09ae
Miguel Heredia Frias <migui.nfk@gmail.com>
2018-11-23 12:00:38 +0100

    Initial Commit

---
+++ appventa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appventa
+//
+//  Created by Miguel Heredia Frias on 23/11/2018.
+//  Copyright © 2018 Miguel Heredia Frias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
