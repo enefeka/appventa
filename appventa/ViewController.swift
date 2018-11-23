34a1ed0967c7f9551848084df1bb42ddecb6aedc
Miguel Heredia Frias <migui.nfk@gmail.com>
2018-11-23 12:45:49 +0100

    Prueba constraints

--- appventa/ViewController.swift
+++ appventa/ViewController.swift
@@ -2,11 +2,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let size = view.bounds.size.width
+        
+        if size > 400 {
+            
+            bottomConstraint.constant = 500
+        
+        }else if size > 350 {
+            
+            bottomConstraint.constant = 200
+            
+        }else{
+            
+            bottomConstraint.constant = 170
+        }
+        
+    
     }
-
+    
 
 }
 
